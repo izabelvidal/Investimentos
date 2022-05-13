a2d857fef59e8454c58fc0fdb788fd8eb438d608
Izabel Vidal <izabelvidal99@gmail.com>
2022-05-13 13:51:04 -0300

    Initial commit

---
+++ src/main/java/br/com/inter/Investimentos/model/domain/EmpresaModel.java
@@ -0,0 +1,27 @@
+package br.com.inter.Investimentos.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "empresa")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Empresa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ticket;
+    private String acao;
+    private Double preco;
+}
+
---
+++ src/main/java/br/com/inter/Investimentos/service/EmpresaService.java
@@ -0,0 +1,2 @@
+package br.com.inter.Investimentos.service;public class EmpresaService {
+}
